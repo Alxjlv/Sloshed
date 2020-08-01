8a33033a9a4cd7e5a4eb36cfbc86c9f24f6ec6b7
Alex Verkerk <alxjlv@gmail.com>
2020-08-01 12:49:55 +1200

    Added error reporting

--- src/gyroscope.js
+++ src/gyroscope.js
@@ -1,18 +1,31 @@
 function gyro(){
-	let gyroscope = new Gyroscope({frequency: 60});
+	let gyroscope = new Gyroscope();
+	gyroscope.start();
 
-	gyroscope.addEventListener('reading', e => {
-		var x = document.getElementById('x');
-		var y = document.getElementById('y');
-		var z = document.getElementById('z');
-
-		x.innerText = "Angular velocity along the X-axis " + gyroscope.x;
-		y.innerText = "Angular velocity along the Y-axis " + gyroscope.y;
-		z.innerHText = "Angular velocity along the Z-axis " + gyroscope.z;
+	gyroscope.onreading = () => {
+		var x = document.getElementById('x').innerText = "Angular velocity along the X-axis " + gyroscope.x;
+		var y = document.getElementById('y').innerText = "Angular velocity along the Y-axis " + gyroscope.y;
+		var z = document.getElementById('z').innerHText = "Angular velocity along the Z-axis " + gyroscope.z;
 
 	  	console.log("Angular velocity along the X-axis " + gyroscope.x);
 	  	console.log("Angular velocity along the Y-axis " + gyroscope.y);
 	  	console.log("Angular velocity along the Z-axis " + gyroscope.z);
-	});
-	gyroscope.start();
+	}
+	gyroscope.onerror = event => {
+		console.log(event.error.name, event.error.message)
+		document.getElementById('error').innerText = event.error.name, event.error.message;
+	}
+}
+
+function gyro2(){
+	let sensor = new Gyroscope();
+sensor.start();
+
+sensor.onreading = () => {
+    console.log("Angular velocity around the X-axis " + sensor.x);
+    console.log("Angular velocity around the Y-axis " + sensor.y);
+    console.log("Angular velocity around the Z-axis " + sensor.z);
+};
+
+sensor.onerror = event => console.log(event.error.name, event.error.message);
 }
