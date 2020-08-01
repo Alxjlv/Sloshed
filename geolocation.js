39ca44214a5126d33358fe19d2377a25ed4566f6
Laura Huijser <laura.huijser@yahoo.com>
2020-08-02 09:43:23 +1200

    Added button back (testing)

--- geolocation.js
+++ geolocation.js
@@ -1,4 +1,4 @@
-function geoFindMe() {
+/*function geoFindMe() {
   
     // Return found location if geolocation works
     function success(position) {
@@ -19,4 +19,36 @@ function geoFindMe() {
       navigator.geolocation.getCurrentPosition(success, error);
     }
 
+  }*/
+
+  function geoFindMe() {
+
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#map-link');
+  
+    mapLink.href = '';
+    mapLink.textContent = '';
+  
+    function success(position) {
+      const latitude  = position.coords.latitude;
+      const longitude = position.coords.longitude;
+  
+      status.textContent = '';
+      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      mapLink.textContent = `Latitude: ${latitude}°, Longitude: ${longitude}°`;
+    }
+  
+    function error() {
+      status.textContent = 'Unable to retrieve your location';
+    }
+  
+    if(!navigator.geolocation) {
+      status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+      status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
   }
+  
+  document.querySelector('#find-me').addEventListener('click', geoFindMe);
