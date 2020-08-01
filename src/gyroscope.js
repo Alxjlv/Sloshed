5b30bfeec86037e1e46be3b7945d73f0b18cd909
Alex Verkerk <alxjlv@gmail.com>
2020-08-01 14:24:00 +1200

    Added stability code to gyroscope.js

--- src/gyroscope.js
+++ src/gyroscope.js
@@ -7,6 +7,7 @@ function gyro(){
 		document.getElementById('y').innerText = "Angular velocity along the Y-axis " + gyroscope.y;
 		document.getElementById('z').innerText = "Angular velocity along the Z-axis " + gyroscope.z;
 
+		document.getElementById('stable').innerText = userIsStable(gyroscope);
 	  	console.log("Angular velocity along the X-axis " + gyroscope.x);
 	  	console.log("Angular velocity along the Y-axis " + gyroscope.y);
 	  	console.log("Angular velocity along the Z-axis " + gyroscope.z);
@@ -15,4 +16,18 @@ function gyro(){
 		console.log(event.error.name, event.error.message)
 		document.getElementById('error').innerText = event.error.name, event.error.message;
 	}
+}
+
+function userIsStable(gyro) { 
+    let velMagThresh = 0.2; // Min Mag.
+    document.getElementById('thresh').innerText = velMagThresh
+    if (getMagnitude(gyro.x, gyro.y, gyro.z) > velMagThresh) return false;
+    else return true;
+}
+
+function getMagnitude(x, y, z) { 
+    let magnitude = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)); // Calculate mag.
+    document.getElementById('magnitude').innerText = magnitude
+    console.log(magnitude);
+    return magnitude;
 }
