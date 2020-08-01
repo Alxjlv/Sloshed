ea7c20157065722710fc7e53e9d1a6476f05f027
Laura Huijser <laura.huijser@yahoo.com>
2020-08-02 09:35:07 +1200

    Removed button and text so all works invisibly

--- geolocation.js
+++ geolocation.js
@@ -1,31 +1,22 @@
 function geoFindMe() {
-
-    const status = document.querySelector('#status');
-    const mapLink = document.querySelector('#map-link');
   
-    mapLink.href = '';
-    mapLink.textContent = '';
-  
+    // Return found location if geolocation works
     function success(position) {
       const latitude  = position.coords.latitude;
       const longitude = position.coords.longitude;
-  
-      status.textContent = '';
-      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: ${latitude}°, Longitude: ${longitude}°`;
+
+      const location = new google.maps.LatLng(latitude, longitude);
+      return location
     }
   
+    // Return location of create camp if there is an error
     function error() {
-      status.textContent = 'Unable to retrieve your location';
+      return new google.maps.LatLng(-36.843410, 174.756973);
     }
   
-    if(!navigator.geolocation) {
-      status.textContent = 'Geolocation is not supported by your browser';
-    } else {
-      status.textContent = 'Locating…';
+    // Attempt to fetch location
+    if(navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(success, error);
     }
-  
+
   }
-  
-  document.querySelector('#find-me').addEventListener('click', geoFindMe);
