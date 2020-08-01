a92cc95f6b7b82719cafec40f447c56ba4340cbc
Alex Verkerk <alxjlv@gmail.com>
2020-08-01 14:03:09 +1200

    Gyroscope now works on mobile

--- src/gyroscope.js
+++ src/gyroscope.js
@@ -6,28 +6,14 @@ function gyro(){
 	gyroscope.onreading = () => {
 		document.getElementById('x').innerText = "Angular velocity along the X-axis " + gyroscope.x;
 		document.getElementById('y').innerText = "Angular velocity along the Y-axis " + gyroscope.y;
-		document.getElementById('z').innerHText = "Angular velocity along the Z-axis " + gyroscope.z;
+		document.getElementById('z').innerText = "Angular velocity along the Z-axis " + gyroscope.z;
 
 	  	console.log("Angular velocity along the X-axis " + gyroscope.x);
 	  	console.log("Angular velocity along the Y-axis " + gyroscope.y);
 	  	console.log("Angular velocity along the Z-axis " + gyroscope.z);
 	}
 	gyroscope.onerror = event => {
-
 		console.log(event.error.name, event.error.message)
 		document.getElementById('error').innerText = event.error.name, event.error.message;
 	}
-}
-
-function gyro2(){
-	let sensor = new Gyroscope();
-sensor.start();
-
-sensor.onreading = () => {
-    console.log("Angular velocity around the X-axis " + sensor.x);
-    console.log("Angular velocity around the Y-axis " + sensor.y);
-    console.log("Angular velocity around the Z-axis " + sensor.z);
-};
-
-sensor.onerror = event => console.log(event.error.name, event.error.message);
 }
