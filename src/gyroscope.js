b9943ccad8879c82c95fdd734395e563397361de
Alex Verkerk <alxjlv@gmail.com>
2020-08-01 12:52:45 +1200

    Added testing message

--- src/gyroscope.js
+++ src/gyroscope.js
@@ -1,17 +1,19 @@
 function gyro(){
+	document.getElementById('x').innerText = "testing"
 	let gyroscope = new Gyroscope();
 	gyroscope.start();
 
 	gyroscope.onreading = () => {
-		var x = document.getElementById('x').innerText = "Angular velocity along the X-axis " + gyroscope.x;
-		var y = document.getElementById('y').innerText = "Angular velocity along the Y-axis " + gyroscope.y;
-		var z = document.getElementById('z').innerHText = "Angular velocity along the Z-axis " + gyroscope.z;
+		document.getElementById('x').innerText = "Angular velocity along the X-axis " + gyroscope.x;
+		document.getElementById('y').innerText = "Angular velocity along the Y-axis " + gyroscope.y;
+		document.getElementById('z').innerHText = "Angular velocity along the Z-axis " + gyroscope.z;
 
 	  	console.log("Angular velocity along the X-axis " + gyroscope.x);
 	  	console.log("Angular velocity along the Y-axis " + gyroscope.y);
 	  	console.log("Angular velocity along the Z-axis " + gyroscope.z);
 	}
 	gyroscope.onerror = event => {
+
 		console.log(event.error.name, event.error.message)
 		document.getElementById('error').innerText = event.error.name, event.error.message;
 	}
