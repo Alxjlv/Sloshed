033d54eba85940f5ed3726a412015033cb84c38f
Di Kun Ong <dikuno10two@gmail.com>
2020-08-02 13:21:38 +1200

    Add gyro handling logic

--- scripts/checker.js
+++ scripts/checker.js
@@ -8,4 +8,31 @@ function loadDummy() {
   }, 500);
 }
 
-window.onload = loadDummy;
+/**
+ * The user must pass a series of checks to get instant access to the search results.
+ * If any of the gates are not passed, the user is redirected to the reaction test.
+ */
+function responsibilityCheck() {
+  startGyro();
+
+  // if there is no gyroscope, instantly redirect
+  if (!errorState()) {
+    window.location.href = "reaction-test.html";
+  }
+
+  // after 5 seconds, show results only if gyro test passed
+  setTimeout(() => {
+    let userState = stopGyro();
+    if (userState) {
+      console.log("User is sloshed");
+      // redirect to react test
+      window.location.href = "reaction-test.html";
+    } else {
+      console.log("User is all goods");
+      loadingSec.classList.toggle("section__show");
+      resultsSec.classList.toggle("section__show");
+    }
+  }, 5000)
+}
+
+window.onload = responsibilityCheck;
