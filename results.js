46a0f37fcf44c71f3637cea7ecbbff3f4ab9e000
RawiriHohepa <54158903+RawiriHohepa@users.noreply.github.com>
2020-08-02 14:06:49 +1200

    workable Places API v2

--- results.js
+++ results.js
@@ -2,16 +2,41 @@
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
-const SEARCH_RADIUS = '1000';
+const SEARCH_RADIUS = '200';
 
-function initMap() {
+async function getLocationsAsync() {
     const currentPos = geoFindMe();
-    retrieveData(currentPos);
+    const locations = await retrieveData(currentPos);
+    console.log(JSON.stringify(locations));
+
+    // nearbySearch();
 }
 
 function geoFindMe() {
-    return new google.maps.LatLng(-33.867, 151.195);
+    return new google.maps.LatLng(-36.842240, 174.756797);
 }
+
+// const encodeGetParams = p =>
+//     Object.entries(p).map(kv => kv.map(encodeURIComponent).join("=")).join("&");
+
+// async function nearbySearch() {
+//     const apiKey = "AIzaSyDUqXF3eNX4my85sgEgdWLl30BSUfyDtso";
+
+//     const parameters = {
+//         key: apiKey,
+//         location: "-36.842240,174.756797",
+//         rankby: "distance",
+//         type: ['bars']
+//     }
+
+//     const url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+//     const params = encodeGetParams(parameters)
+//     const response = await fetch(url + params)
+//     const data = response.json()
+
+//     console.log(data)
+// }
+
 
 function retrieveData(currentPos) {
     // TODO find way to make API call work without this
@@ -20,40 +45,47 @@ function retrieveData(currentPos) {
         zoom: 15
     });
 
-    const request = {
+    const requestBars = {
         location: currentPos,
         radius: SEARCH_RADIUS,
-        type: ['liquor_store']
+        // rankby: "distance",
+        type: ['bar']
     };
 
     const service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
-}
+    //service.nearbySearch(requestLiquor, callbackLiquor);
+    return new Promise((resolve, reject) => {
+        service.nearbySearch(requestBars, (results, status) => {
+            let locations = [];
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                for (let i = 0; i < results.length; i++) {
+                    // console.log(results)
+                    const result = {
+                        name: results[i].name,
 
-function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-        //   for (let i = 0; i < results.length; i++) {
-        //     console.log(results[i]);
-        //   }
+                        // TODO remove deprecated field open_now, try to make isOpen() work
+                        // TODO if possible add something to give actual opening hours
+                        //is_open_now: results[i].opening_hours.open_now,
 
-        const result = {
-            name: results[0].name,
+                        // TODO add something to get address for given LatLng
+                        is_open_now: results[i].opening_hours ? results[i].opening_hours.open_now : false,
 
-            // TODO remove deprecated field open_now, try to make isOpen() work
-            // TODO if possible add something to give actual opening hours
-            is_open_now: results[0].opening_hours.open_now,
+                        lat: results[i].geometry.location.lat().toString(),
+                        lng: results[i].geometry.location.lng().toString(),
+                        address: results[i].vicinity ? results[i].vicinity : 'Unknown',
+                        // type: results[i].types,
 
-            // TODO add something to get address for given LatLng
-            address: results[0].geometry.location.lat().toString() + ',' + results[0].geometry.location.lng().toString(),
+                        // TODO add something to calculate distance from current location
+                        distance: 0
+                    };
+                    locations.push(result);
+                }
 
-            // TODO add something to calculate distance from current location
-            distance: 0
-        };
+                resolve(locations)
 
-        displayResults(result);
-    }
-}
+            }
+        });
 
-function displayResults(result) {
-    console.log(JSON.stringify(result));
+    })
+
 }
